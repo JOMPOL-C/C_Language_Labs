aa9fc8c1e4583559fb1a9ed69437a5540087deee
Jompol CHUENRAROM <78734499+JOMPOL-C@users.noreply.github.com>
2025-09-29 03:31:04 +0700

    Add files via upload

---
+++ Lab1/Lab02_P03.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+main()
+{
+	int x,y;
+	printf("Plese Enter x:");
+	scanf("%c",&x);
+	printf("Plese Enter y:");
+	scanf("&d",&x);
+	printf("x+y = %d\n");
+	printf("x mod y = %d\n", x%y);
+	printf("-x = %d\n",(-1)*x);
+	printf("-x = %d",-x);
+}
